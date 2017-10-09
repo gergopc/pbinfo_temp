729fb61f8dfde586dd1979bbc90e93f957cd2a55
gergopc <gergoesjanka@gmail.com>
2017-10-09 21:52:10 +0300

    pbinfo 253

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main(){
+    int n;
+    cin>>n;
+    if(n%2 == 0) cout<<n+2<<' '<<n+4;
+    else cout<<n+1<<' '<<n+3;
     return 0;
 }
