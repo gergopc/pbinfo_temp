3ff2fd1a3525ace351e36a0386f279d91aa7982c
gergopc <gergoesjanka@gmail.com>
2017-10-18 10:25:36 +0300

    1002 paginare

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
-#include <cmath>
-
 using namespace std;
+int main ()
+{int n;
+ cin>>n;
+ if (n%792==0) cout<<n/792<<' '<<0;
+ else{
+        if (n%792<=9) cout<<(n/792+1)<<' '<<(n%792);
+       else{
+           if (n%792<=189) {
+               if ((n%792-9)%2==0) cout<<(n/792+1)<<' '<<((n%792-9)/2+9);
+                              else cout<<"IMPOSIBIL";
+ }
+             else{
+                    if ((n%792-189)%3==0) cout<<(n/792+1)<<' '<<((n%792-189)/3+99);
+                   else cout<<"IMPOSIBIL";
+             }
+             }
+    }
 
-int main(){
-  //deklaralasok
-  long int h, sz;
-
- //beolvasas
- cin>>h>>sz;
- //megoldas
-
-  //kiiratas
- cout<<e;
-  return ;
-}
+ }
