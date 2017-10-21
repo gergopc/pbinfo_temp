936a90fed2c3e103dd37f630922d2a74638b8dc9
gergopc <gergoesjanka@gmail.com>
2017-10-21 10:07:50 +0300

    timp1

--- main.cpp
+++ main.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main ()
-{int n;
- cin>>n;
- if (n%792==0) cout<<n/792<<' '<<0;
- else{
-        if (n%792<=9) cout<<(n/792+1)<<' '<<(n%792);
-       else{
-           if (n%792<=189) {
-               if ((n%792-9)%2==0) cout<<(n/792+1)<<' '<<((n%792-9)/2+9);
-                              else cout<<"IMPOSIBIL";
- }
-             else{
-                    if ((n%792-189)%3==0) cout<<(n/792+1)<<' '<<((n%792-189)/3+99);
-                   else cout<<"IMPOSIBIL";
-             }
-             }
-    }
+{
+ int h1, m1, x, y, h2, m2;
+ cin>>h1>>m1>>x>>y; //7 11 5 34
+ if(x <= 10000 && y < 79){
+    h2 = h1 + x; //h2 = 7 + 5 = 12
+    m2 = m1 + y;
+    if(h2 > 23) //m2 = 11 + 34 = 45
+    h2 = m2/60; //h2 = 0
+    m2 = m2%60; //m2 = 45
 
+    h2 = h2 % 24;
  }
+
+ cout<<h2<<' '<<m2;
+}
