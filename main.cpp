743e2df4199919d159855dbc5fce0aeac8ffe67a
gergopc <gergoesjanka@gmail.com>
2017-10-21 11:29:26 +0300

    1002

--- main.cpp
+++ main.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 
 using namespace std;
-int main ()
-{int n;
+int main (){
+int n;
 cin>>n;
 if(n != 0){
-if (n%792==0) cout<<n/792<<' '<<0;
-else{
-if (n%792<=9) cout<<(n/792+1)<<' '<<(n%792);
-else{
-if (n%792<=189) {
-if ((n%792-9)%2==0) cout<<(n/792+1)<<' '<<((n%792-9)/2+9);
-else cout<<"IMPOSIBIL";
-}
-else{
-if ((n%792-189)%3==0) cout<<(n/792+1)<<' '<<((n%792-189)/3+99);
-else cout<<"IMPOSIBIL";
+  if (n%792==0) cout<<n/792<<' '<<0;
+  else{
+     if (n%792<=9) cout<<(n/792+1)<<' '<<(n%792);
+     else{
+  if (n%792<=189) {
+     if ((n%792-9)%2==0) cout<<(n/792+1)<<' '<<((n%792-9)/2+9);
+    else cout<<"IMPOSIBIL";
+ }else{
+     if ((n%792-189)%3==0) cout<<(n/792+1)<<' '<<((n%792-189)/3+99);
+     else cout<<"IMPOSIBIL";
 }
 }
 }
