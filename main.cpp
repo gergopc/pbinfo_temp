17f618e4a884ff62a62149d0b0e5fe111253700b
gergopc <gergoesjanka@gmail.com>
2017-11-27 09:53:49 +0200

    108

--- main.cpp
+++ main.cpp
@@ -17,3 +17,26 @@ for(int i=1; i<n; i++){
 cout<<maxi<<' '<<e;
 
 }
+//108
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+long long int n, m, a, b;
+cin>>n;
+n=m;
+while(n>0){
+    a=a*10+n%10;
+    n=n/100;
+}
+b=m%10;
+m=m/10;
+while(m>0){
+    b=b*10+m%10;
+    m=m/100;
+}
+cout<<a<<endl<<b;
+}
+
